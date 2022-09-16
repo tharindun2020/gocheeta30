265b2b3df97996ce946bda3710deb2d717505f8c
unknown <dehigaspitiyagedontharindu@gmail.com>
2022-09-16 21:28:43 +0530

    create Remove Typeveh_Remove User

---
+++ src/java/sevletz/RemoveUser.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sevletz;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+public class RemoveUser extends HttpServlet {
+
+  @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        
+        Session ses = Utill.NewHibernateUtil.getSessionFactory().openSession();
+        Transaction tc = ses.beginTransaction();
+        
+        int vehid = Integer.parseInt(request.getParameter("vehid"));
+        POJO.Users user_single = (POJO.Users) ses.load(POJO.Users.class, vehid);
+
+        ses.delete(user_single);
+        
+        tc.commit();
+        ses.close();
+        out.write("ok");
+
+    }
+}
